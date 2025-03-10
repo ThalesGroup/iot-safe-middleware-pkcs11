6df7655034f1460cb826d3fedf4e14a11e43de84
asyarifstudio <syarif.ahmad@thalesgroup.com>
2025-03-10 10:16:55 +0800

    read client certificate from iot-safe-applet

--- examples/tls-client/src/client.c
+++ examples/tls-client/src/client.c
@@ -8,16 +8,26 @@
 #include <openssl/engine.h>
 #define SERVER_IP "127.0.0.1"
 #define PORT 4443
+#define CERT_ID_LEN 20
 
-void init_openssl() {
+ENGINE *engine;
+const char *engine_id = "pkcs11";
+const char *pkcs11_key_id = "pkcs11:id=01;type=private";
+const char client_cert_id[CERT_ID_LEN] = {0x77, 0x2C, 0xBC, 0xA7, 0xB8, 0xD7, 0x7C, 0x7D, 0xF5, 0x7F, 0xA7, 0xE3, 0x90, 0x2F, 0x73, 0x10, 0x71, 0x4C, 0x6F, 0x36};
+const char ca_cert_id[CERT_ID_LEN] = {0xED, 0xD7, 0xA9, 0x78, 0x41, 0xB5, 0x24, 0xC1, 0xD6, 0x54, 0xC0, 0x8C, 0xED, 0x2D, 0xF9, 0x5C, 0xAD, 0xCF, 0xFF, 0x39};
+const char *pin = "0000";
+
+void init_openssl()
+{
     SSL_load_error_strings();
     OpenSSL_add_ssl_algorithms();
 }
-
-SSL_CTX *create_context() {
+SSL_CTX *create_context()
+{
     const SSL_METHOD *method = SSLv23_client_method();
     SSL_CTX *ctx = SSL_CTX_new(method);
-    if (!ctx) {
+    if (!ctx)
+    {
         perror("Unable to create SSL context");
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
@@ -25,24 +35,22 @@ SSL_CTX *create_context() {
     return ctx;
 }
 
-void configure_context(SSL_CTX *ctx) {
-    ENGINE *engine;
-    const char *engine_id = "pkcs11";
-    const char *pkcs11_key_id = "pkcs11:id=01;type=private";
-    const char *pkcs11_cert_id = "pkcs11:id=01;type=cert";
-    const char *pin = "0000";
+void configure_context(SSL_CTX *ctx)
+{
 
     // Load the PKCS#11 engine
     ENGINE_load_dynamic();
     engine = ENGINE_by_id(engine_id);
-    if (!engine) {
+    if (!engine)
+    {
         fprintf(stderr, "Failed to load PKCS#11 engine\n");
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
 
     // Initialize the engine
-    if (!ENGINE_init(engine)) {
+    if (!ENGINE_init(engine))
+    {
         fprintf(stderr, "Failed to initialize PKCS#11 engine\n");
         ERR_print_errors_fp(stderr);
         ENGINE_free(engine);
@@ -50,36 +58,58 @@ void configure_context(SSL_CTX *ctx) {
     }
 
     // Set the engine as the default for all cryptographic operations
-    if (!ENGINE_set_default(engine, ENGINE_METHOD_ALL)) {
+    if (!ENGINE_set_default(engine, ENGINE_METHOD_ALL))
+    {
         fprintf(stderr, "Failed to set PKCS#11 engine as default\n");
         ERR_print_errors_fp(stderr);
         ENGINE_free(engine);
         exit(EXIT_FAILURE);
     }
 
-    ENGINE_ctrl_cmd(engine,"PIN",0,pin,NULL,0);
+    // set default PIN
+    ENGINE_ctrl_cmd(engine, "PIN", 0, pin, NULL, 0);
+
+    // enable log
+    // ENGINE_ctrl_cmd(engine, "VERBOSE", 0, NULL, NULL, 0);
 
     // Load and trust the CA certificate for server verification
-    if (SSL_CTX_load_verify_locations(ctx, "ca-cert.pem", NULL) <= 0) {
+    if (SSL_CTX_load_verify_locations(ctx, "ca-cert.pem", NULL) <= 0)
+    {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
 
-    if (SSL_CTX_use_certificate_file(ctx, "client-cert.pem", SSL_FILETYPE_PEM) <= 0 ) {
+    // retrieve client certificate
+    char pkcs11_cert_id[40] = "pkcs11:id=00000000000000000000;type=cert";
+    for (int i = 0; i < CERT_ID_LEN; i++)
+    {
+        pkcs11_cert_id[i + 10] = client_cert_id[i];
+    }
+    struct
+    {
+        const char *s_slot_cert_id;
+        X509 *cert;
+    } p = {pkcs11_cert_id, NULL};
+
+    ENGINE_ctrl_cmd(engine, "LOAD_CERT_CTRL", 0, &p, NULL, 0);
+    if (SSL_CTX_use_certificate(ctx, p.cert) <= 0)
+    {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
-    
+
     // Load private key from PKCS#11
     EVP_PKEY *pkey = ENGINE_load_private_key(engine, pkcs11_key_id, NULL, NULL);
-    if (!pkey) {
+    if (!pkey)
+    {
         fprintf(stderr, "Failed to load private key from PKCS#11\n");
         ERR_print_errors_fp(stderr);
         ENGINE_free(engine);
         exit(EXIT_FAILURE);
     }
 
-    if (SSL_CTX_use_PrivateKey(ctx, pkey) <= 0) {
+    if (SSL_CTX_use_PrivateKey(ctx, pkey) <= 0)
+    {
         fprintf(stderr, "Failed to use the private key\n");
         ERR_print_errors_fp(stderr);
         EVP_PKEY_free(pkey);
@@ -87,32 +117,27 @@ void configure_context(SSL_CTX *ctx) {
         exit(EXIT_FAILURE);
     }
 
-   
-    
-
-
-
     // Verify that the private key matches the certificate
-    if (!SSL_CTX_check_private_key(ctx)) {
+    if (!SSL_CTX_check_private_key(ctx))
+    {
         fprintf(stderr, "Private key does not match the certificate public key\n");
         exit(EXIT_FAILURE);
     }
 
     // Enforce server certificate verification
     SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
-
-    // Free the engine
-    ENGINE_free(engine);
 }
 
-int main() {
+int main()
+{
     int sockfd;
     struct sockaddr_in addr;
     char buffer[1024] = "Hello, TLS Server!";
-    
+
     init_openssl();
     SSL_CTX *ctx = create_context();
     configure_context(ctx);
+
     SSL *ssl = SSL_new(ctx);
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -120,15 +145,19 @@ int main() {
     addr.sin_port = htons(PORT);
     inet_pton(AF_INET, SERVER_IP, &addr.sin_addr);
 
-    if (connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+    if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
 
     SSL_set_fd(ssl, sockfd);
-    if (SSL_connect(ssl) <= 0) {
+    if (SSL_connect(ssl) <= 0)
+    {
         ERR_print_errors_fp(stderr);
-    } else {
+    }
+    else
+    {
         printf("Connected and verified the server\n");
         SSL_write(ssl, buffer, strlen(buffer));
         SSL_read(ssl, buffer, sizeof(buffer));
@@ -139,6 +168,7 @@ int main() {
     SSL_free(ssl);
     close(sockfd);
     SSL_CTX_free(ctx);
+    ENGINE_free(engine);
     EVP_cleanup();
     return 0;
 }
